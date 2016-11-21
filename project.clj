8c63686291a38768f496806915718a6a096f3c6c
Maximilian Weber <weber.maximilian@googlemail.com>
2016-11-21 18:23:31 +0100

    Version 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sv/timbre.gcloud.logging "0.1.1-SNAPSHOT"
+(defproject sv/timbre.gcloud.logging "0.1.1"
   :description "A timbre appender for the Google Cloud Logging service"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
