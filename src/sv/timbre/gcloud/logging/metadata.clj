66f0ec6e557dc41f62820860f69a745c6cba5559
Maximilian Weber <weber.maximilian@googlemail.com>
2016-11-21 18:11:45 +0100

    Bugfix: corrected metadata fetching and preparing.

--- src/sv/timbre/gcloud/logging/metadata.clj
+++ src/sv/timbre/gcloud/logging/metadata.clj
@@ -1,4 +1,6 @@
-(ns sv.timbre.gcloud.logging.metadata)
+(ns sv.timbre.gcloud.logging.metadata
+  (:require [clj-http.lite.client :as lc]
+            [cheshire.core :as ch]))
 
 (def instance-metadata-sample
   {:zone "projects/123/zones/europe-west1-b"
@@ -15,20 +17,32 @@
   (second (re-find #".*\.(.*?)\.internal$" hostname)))
 
 (defn prepare-log-metadata [instance-metadata]
-  {:project_id (extract-project-id (:hostname instance-metadata))
-   :zone (extract-zone (:zone instance-metadata))
-   :instance_id (str (:id instance-metadata))})
+  (let [project-id (extract-project-id (:hostname instance-metadata))]
+    {:logName (str "projects/"
+                   project-id
+                   "/logs/clj")
+     :resource {:type "gce_instance"
+                :labels
+                {:zone (extract-zone (:zone instance-metadata))
+                 :instance_id (str (:id instance-metadata))}}}))
 
 (def instance-metadata-request
   {:request-method :get
    :url "http://metadata.google.internal/computeMetadata/v1/instance/"
    :query-params {:recursive true}
-   :headers {"Metadata-Flavor" "Google"}
-   :as :json})
+   :headers {"Metadata-Flavor" "Google"}})
 
-(defn get-log-metadata
+(defn get-entry-metadata*
   "Only works on a Google Compute Engine (GCE) instance (since
    http://metadata.google.internal/ is only available there)"
   [request-fn]
   (let [response (request-fn instance-metadata-request)]
     (prepare-log-metadata (:body response))))
+
+(defn get-entry-metadata []
+  (get-entry-metadata*
+   (fn [req]
+     (update
+      (lc/request req)
+      :body
+      #(ch/parse-string % true)))))
